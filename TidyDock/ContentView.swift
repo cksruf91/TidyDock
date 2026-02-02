ccbc00c47cd0e1e3ec6ea01f4334e6a4015e96cb
chan <cksruf91@gmail.com>
2026-02-02 16:56:41 +0900

    init

---
+++ TidyDock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TidyDock
+//
+//  Created by 박찬결 on 2/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TidyDock/TidyDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  TidyDockApp.swift
+//  TidyDock
+//
+//  Created by 박찬결 on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct TidyDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
