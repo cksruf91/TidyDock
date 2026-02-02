db4ca59eaf81eb1afc8310898ccdf2969a8e6a36
chan <cksruf91@gmail.com>
2026-02-02 17:00:24 +0900

    init

---
+++ TidyDockUITests/TidyDockUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TidyDockUITestsLaunchTests.swift
+//  TidyDockUITests
+//
+//  Created by 박찬결 on 2/2/26.
+//
+
+import XCTest
+
+final class TidyDockUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
